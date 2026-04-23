31863404ccb63b15af85ccf82385956e65314336
AQStarLilly <180299378+AQStarLilly@users.noreply.github.com>
2026-04-23 12:25:31 -0300

    working on puck (pseudocoded)

---
+++ Assets/Kieran's Scripts/PuckBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PuckBehaviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Set up a magnet for puck that connects to tagged player
+         * Attaches right in front of the player for now, will move later whe nstick gets added
+         * 
+         * if collidor == player (stick object ?) 
+         * 
+         * 
+         * 
+         */
+    }
+}
