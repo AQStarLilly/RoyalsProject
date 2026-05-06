7216d0a59967dea90af8e938ed64bab41404be5d
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-05-06 19:08:53 -0300

    win tracker basic
    it tracks how many wins you got after you score five goals

---
+++ Assets/Gage's Scripts/TournementManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class TournementManager : MonoBehaviour
+{
+    private GameManager gameManager;
+    
+    public int CurrentWins = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = FindFirstObjectByType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameManager.team1Score >= 5)
+        {
+            DisplayVictory();
+            gameManager.ResetScores();
+            CurrentWins += 1;
+        }
+        else if(gameManager.team2Score >= 5)
+        {
+            DisplayLoss();
+            gameManager.ResetScores();
+        }
+
+        if(CurrentWins == 2)
+        {
+            DisplayChampions();
+            gameManager.ResetScores();
+        }
+    }
+
+    
+    void DisplayChampions()
+    {
+        Debug.Log("Champs");
+    }
+    void DisplayVictory()
+    {
+        Debug.Log("Winner");
+    }
+    void DisplayLoss()
+    {
+        Debug.Log("Loss");
+    }
+}
